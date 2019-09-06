f0dc94ef5de1815aab006dd7548397f62a8bd38f
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-06 10:44:50 -0700

    kernel: Use prebuilt binaries
    perl, lz4c, and a bunch of other binaries are not allowed anymore.
    So we need to provide our own.
    
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -1,3 +1,5 @@
+PATH_OVERRIDE_SOONG := $(shell echo $(TOOLS_PATH_OVERRIDE) | sed -e 's|$$|$$$$|g')
+
 # Add variables that we wish to make available to soong here.
 ifneq (,$(wildcard $(OUT_DIR)/.path_interposer_origpath))
 ORIG_PATH := $(shell cat $(OUT_DIR)/.path_interposer_origpath)
@@ -9,6 +11,7 @@ EXPORT_TO_SOONG := \
     KERNEL_BUILD_OUT_PREFIX \
     KERNEL_CROSS_COMPILE \
     KERNEL_MAKE_FLAGS \
+    PATH_OVERRIDE_SOONG \
     TARGET_KERNEL_CONFIG \
     TARGET_KERNEL_SOURCE \
     MAKE_PREBUILT \
